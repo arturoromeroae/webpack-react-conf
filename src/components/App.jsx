1053e222ba2aed0e96c505a84777457288ef7a19
Arturo Romero <aromero@aeonlinesolutions.com>
2022-03-02 15:19:03 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Hello World React!!! 🙋‍♂️</h1>
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html' 
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        open: true,
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
+    }
+}
